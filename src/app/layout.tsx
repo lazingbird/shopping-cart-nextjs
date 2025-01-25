029b4895fbb6289d39c5cfe8dc1427318c7742b3
lazingbird <lazingbird@protonmail.com>
2025-01-25 13:13:37 -0300

    prepare basic next.js project

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html>
+      <body>
+        <main>
+          {children}
+        </main>
+      </body>
+    </html>
+  )
+}
